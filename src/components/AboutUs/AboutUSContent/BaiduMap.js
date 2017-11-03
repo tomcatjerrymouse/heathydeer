52d5f808934000c15d3b1fbea1a1706d220caefc
18408285683 <18408285683@163.com>
2017-11-03 16:10:01 +0800

    1

--- src/components/AboutUs/AboutUSContent/BaiduMap.js
+++ src/components/AboutUs/AboutUSContent/BaiduMap.js
@@ -7,7 +7,7 @@ class BaiduMap extends React.Component {
         var map = new BMap.Map("allmap"); // 创建Map实例
         map.centerAndZoom(new BMap.Point(113.951342, 22.562368), 17); // 初始化地图,设    置中心点坐标和地图级别
         map.addControl(new BMap.MapTypeControl()); //添加地图类型控件
-        map.setCurrentCity("北京"); // 设置地图显示的城市 此项是必须设置的
+        map.setCurrentCity("深圳"); // 设置地图显示的城市 此项是必须设置的
         map.enableScrollWheelZoom(true); //开启鼠标滚轮缩放
         var point = new BMap.Point(113.9495, 22.561699);
         map.centerAndZoom(point, 17);
